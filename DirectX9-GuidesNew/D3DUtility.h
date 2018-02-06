65f9fab864d31d71f57fba77bfd0599270176a85
DESKTOP-OI3UGCF\catze <catzeromeio@hotmail.com>
2018-02-06 09:35:19 +0800

    create a basic window

---
+++ DirectX9-GuidesNew/D3DUtility.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <d3dx9.h>
+
+namespace d3d
+{
+	int ScreenHight = 800;
+	int ScreenWeight = 800;
+}
---
+++ DirectX9-GuidesNew/Main.cpp
@@ -0,0 +1,96 @@
+#include<windows.h>
+#include "stdafx.h"
+#include "D3DUtility.h"
+using namespace d3d;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	//register the window class
+	const wchar_t CLASS_NAME[] = L"DirectX9 Guides Window Class";
+
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	//create the window
+	HWND hwnd = CreateWindowEx
+	(
+		0,
+		CLASS_NAME,
+		L"Learn to program windows",
+		WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+		0, 0, ScreenHight, ScreenWeight,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	//InitD3D(hwnd);
+	//SetUp();
+
+	static float lastTime = (float)timeGetTime();
+
+	//run the message loop
+	MSG msg = {};
+	while (true)
+	{
+		float currentTime = (float)timeGetTime();
+
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+		//	CleanUp();
+			return 0;
+		}
+
+		float deltaTime = (currentTime - lastTime)*0.001f;
+		//Display(deltaTime);
+
+		lastTime = currentTime;
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+
+		//FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+		EndPaint(hwnd, &ps);
+		return 0;
+	}
+	default:
+	{
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	}
+}
